9030f51e9832ab4704da1701c9ca666639e77762
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-12 05:26:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,7 +47,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   with_bbw = []
   holiday_hash.each do |season, holiday|
-    bbq << holiday if holiday.values.include?("BBQ")
+    with_bbq << holiday if holiday.values.include?("BBQ")
   end
   with_bbw
 end
